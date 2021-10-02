3ab3f155fabcefa7e9f588b7fb50a9ebb7b7aa39
Jaime <jaimecr72@gmail.com>
2021-10-02 11:23:47 +0000

    primera versión

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let symbols = { 0: "♦", 1: "♥", 2: "♠", 3: "♣" };
+  let colors = { 0: "black", 1: "red", 2: "red", 4: "black" };
+  let i = Math.floor(Math.random() * 4);
+  let n = Math.floor(Math.random() * 12) + 1;
+
+  let divsymbols = document.querySelectorAll(".symbol");
+  divsymbols[0].innerHTML = `<span style="color:${colors[i]}">${symbols[i]}</span>`;
+  divsymbols[1].innerHTML = `<span style="color:${colors[i]}">${symbols[i]}</span>`;
+  document.querySelector(
+    ".number"
+  ).innerHTML = `<span style="color:${colors[i]}">${n}</span>`;
 };
