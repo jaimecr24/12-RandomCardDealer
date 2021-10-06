16890fc81dc7791f0b178d5e69e697e7133d0a49
Jaime <jaimecr72@gmail.com>
2021-10-06 02:50:19 +0000

    color-suite applied to card

--- src/app.js
+++ src/app.js
@@ -14,14 +14,14 @@ let inputh = document.querySelector("#hcard");
 window.onload = generateCard;
 btn.addEventListener("click", generateCard); //Change card onclick event.
 
-inputw.addEventListener("input", function(event) {
+inputw.addEventListener("change", function(event) {
   if (inputw.validity.valid)
     card.setAttribute(
       "style",
       `width:${inputw.value}px; height:${card.clientHeight}px;`
     );
 });
-inputh.addEventListener("input", function(event) {
+inputh.addEventListener("change", function(event) {
   if (inputh.validity.valid)
     card.setAttribute(
       "style",
@@ -32,14 +32,17 @@ inputh.addEventListener("input", function(event) {
 function generateCard() {
   let symbols = { 0: "♦", 1: "♥", 2: "♠", 3: "♣" };
   let colors = { 0: "red", 1: "red", 2: "black", 3: "black" };
-  let letters = { 11: "J", 12: "Q", 13: "K" };
+  let letters = { 1: "A", 11: "J", 12: "Q", 13: "K" };
   let i = Math.floor(Math.random() * 4);
   let n = Math.floor(Math.random() * 12) + 1;
 
+  let divCard = document.querySelector(".card");
+  divCard.style.color = colors[i];
+
   let divsymbols = document.querySelectorAll(".symbol");
-  divsymbols[0].innerHTML = `<span style="color:${colors[i]}">${symbols[i]}</span>`;
-  divsymbols[1].innerHTML = `<span style="color:${colors[i]}">${symbols[i]}</span>`;
-  document.querySelector(".number").innerHTML = `<span style="color:${
-    colors[i]
-  }">${n < 11 ? n : letters[n]}</span>`;
+  divsymbols[0].innerHTML = `<span>${symbols[i]}</span>`;
+  divsymbols[1].innerHTML = `<span>${symbols[i]}</span>`;
+  document.querySelector(".number").innerHTML = `<span>${
+    n < 2 || n > 10 ? letters[n] : n
+  }</span>`;
 }
